a47f296a61dad80270feff070fef5f5ec00733ae
orcadehub <orcadehub2@gmail.com>
2024-12-06 21:21:32 +0530

    test

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 3300;
 const app = express();
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// require("dotenv").config();
 
-// const MONGO_URI = process.env.MONGO_URI;
-// const PORT = process.env.PORT || 3300;
+app.use(cors());
 
-// app.use(cors());
+// JSON middleware for Express
+app.use(express.json());
 
-// // JSON middleware for Express
-// app.use(express.json());
+// Set strictQuery to avoid warnings
+mongoose.set("strictQuery", true);
 
-// // Set strictQuery to avoid warnings
-// mongoose.set("strictQuery", true);
+// MongoDB Connection
+mongoose
+  .connect(MONGO_URI, {
+    serverSelectionTimeoutMS: 15000, // 15 seconds
+  })
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-// // MongoDB Connection
-// mongoose
-//   .connect(MONGO_URI, {
-//     serverSelectionTimeoutMS: 15000, // 15 seconds
-//   })
-//   .then(() => console.log("Successfully connected to MongoDB"))
-//   .catch((err) => console.error("Error connecting to MongoDB:", err));
-
-// // Include models and routes
-// require("./models/user_model");
-// require("./models/product_model");
-// app.use(require("./routes/user_route"));
-// // app.use(require("./routes/user_related_route"));
-// app.use(require("./routes/product_route"));
+// Include models and routes
+require("./models/user_model");
+require("./models/product_model");
+app.use(require("./routes/user_route"));
+// app.use(require("./routes/user_related_route"));
+app.use(require("./routes/product_route"));
 
 // Basic route to check if server is running
 app.get("/", (req, res) => {
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
 });
 
 // Start Express server
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
