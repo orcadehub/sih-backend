b0e62de1fae956c02d27d00d381855c3b2b36155
orcadehub <orcadehub2@gmail.com>
2024-12-06 21:16:29 +0530

    test

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+// const { Telegraf } = require("telegraf");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+// const User = require("./models/user_model");
 require("dotenv").config();
 
 const MONGO_URI = process.env.MONGO_URI;
+// const BOT_TOKEN = process.env.BOT_TOKEN;
+//const WEBHOOK_URL = `${process.env.WEBHOOK_URL}/bot${BOT_TOKEN}`; // Fixed URL format
+// const NODE_ENV = process.env.NODE_ENV || "development";
 const PORT = process.env.PORT || 3300;
+const WEB_URI = process.env.WEB_URI;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://www.thewhiteshark.io",
+      "https://8e4f-175-101-143-151.ngrok-free.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors()); // Handle preflight requests for all routes
 
 // JSON middleware for Express
 app.use(express.json());
@@ -27,9 +46,10 @@ mongoose
 require("./models/user_model");
 require("./models/product_model");
 app.use(require("./routes/user_route"));
-// app.use(require("./routes/user_related_route"));
 app.use(require("./routes/product_route"));
-
+// app.use(require("./routes/task_route"));
+// app.use(require("./routes/bot_route"));
+// require("./cronjobs");
 // Basic route to check if server is running
 app.get("/", (req, res) => {
   res.send("Server is working nov-16 4pm");
